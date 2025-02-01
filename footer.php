425ef28239be168545af89dc0981b89fc9af4559
TryoTrix <dan93@gmx.ch>
2025-02-01 19:54:02 +0100

    v3.2

---
+++ footer.php
@@ -0,0 +1,3 @@
+<footer data-aos="fade-up">
+  <p>© 2025 <a href="https://nachhilfe-lotusacademy.ch">Nachhilfe</a> von Lotus Academy | <a href="https://nachhilfe-lotusacademy.ch">Die Schweizer Nachhilfe</a> | <a href="https://www.nachhilfe-lotusacademy.ch/nachhilfe/standorte/stgallen">Nachhilfe St. Gallen</a> | <a href="https://www.nachhilfe-lotusacademy.ch/nachhilfe/standorte/winterthur">Nachhilfe Winterthur</a>  </p>
+</footer>
